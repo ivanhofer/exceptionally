10b84534174d03119b505a7f3d831c3d8783aeca
Ivan Hofer <ivan.hofer@outlook.com>
2022-10-17 07:44:36 +0200

    add type tests for `assertSuccess` and `assertException`

---
+++ test/assertException.test.types.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { assertException, type Exception, exception, success } from './index.js'
+
+{
+	const e = exception('data')
+
+	assertException(e)
+
+	const eResult: Exception<string> = e
+}
+
+{
+	const s = success('data')
+
+	// @ts-expect-error should not accept Success type
+	assertException(s)
+
+	// @ts-expect-error should be of type never
+	s.toString()
+}
+
+{
+	const e = exception('data')
+	const s = success(123)
+	const r = Math.random() > 0.5 ? s : e
+
+	// @ts-expect-error should not accept Success type
+	assertException(r)
+
+	const rResult: Exception<string> = e
+}
+
+{
+	const o = { isSuccess: false, isException: true }
+
+	// @ts-expect-error should not accept arbitrary
+	assertException(o)
+}
