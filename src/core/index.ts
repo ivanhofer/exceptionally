919ede1be0dc50e67ffa63c7c81ffa6b1ebde6e1
Ivan Hofer <ivan.hofer@outlook.com>
2023-01-11 10:15:18 +0100

    split package into `core` and `assert`

---
+++ src/core/index.ts
@@ -0,0 +1,70 @@
+/* eslint-disable symbol-description */
+
+const exceptionally = Symbol()
+
+type Inverted<Success extends boolean> = Success extends true ? false : true
+
+export class Exceptionally {
+	static [Symbol.hasInstance](object: unknown) {
+		return (object as Record<string, unknown> | undefined)?.exceptionally === exceptionally
+	}
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+type GetDataFn<Data> = () => Data
+
+export type ExceptionallyResult<Success extends boolean, Data> = Success extends true ? SuccessOf<Data>
+	: ExceptionOf<Data>
+
+type SuccessOf<Data> = GetDataFn<Data> & Exceptionally & { isSuccess: true; isException: false }
+type ExceptionOf<Data> = GetDataFn<Data> & Exceptionally & { isSuccess: false; isException: true }
+
+type Wrap<Success extends boolean, Data> = Data extends ExceptionallyResult<boolean, unknown> ? Data
+	: ExceptionallyResult<Success, Data>
+
+const wrap = <Success extends boolean, Data>(
+	success: Success,
+	data: Data,
+): Data extends ExceptionallyResult<boolean, unknown> ? Data : ExceptionallyResult<Success, Data> =>
+	(data instanceof Exceptionally
+		? data
+		: Object.assign(
+			() => data,
+			{
+				exceptionally,
+				isSuccess: success as Success,
+				isException: !success as Inverted<Success>,
+			},
+		)) as Data extends ExceptionallyResult<boolean, unknown> ? Data : ExceptionallyResult<Success, Data>
+
+// ----------------
+
+export type Success<Data> = Wrap<true, Data>
+
+export const success = <Data = undefined>(
+	...data: undefined extends Data ? [] | [Data] : [Data]
+): Data extends ExceptionallyResult<boolean, unknown> ? Data : Success<Data> =>
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	wrap(true, ...data as [Data]) as any
+
+// ----------------
+
+export type Exception<Data> = Wrap<false, Data>
+
+export const exception = <Data = undefined>(
+	...data: undefined extends Data ? [] | [Data] : [Data]
+): Data extends ExceptionallyResult<boolean, unknown> ? Data : Exception<Data> =>
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	wrap(false, ...data as [Data]) as any
+
+// --------------------------------------------------------------------------------------------------------------------
+
+export type ExtractDataType<Result extends ExceptionallyResult<boolean, unknown>> = Result extends
+	ExceptionallyResult<boolean, infer Data> ? Data : never
+
+export type ExtractSuccessType<Result extends ExceptionallyResult<boolean, unknown>> = Result extends
+	ExceptionallyResult<true, infer Data> ? Success<Data> : never
+
+export type ExtractExceptionType<Result extends ExceptionallyResult<boolean, unknown>> = Result extends
+	ExceptionallyResult<false, infer Data> ? Exception<Data> : never
